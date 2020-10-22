db00ca0b53d3773972d70c02a43478d35a6d667f
Maja <maja669e@stud.kea.dk>
2020-10-22 02:47:18 +0200

    tilføjet kode

--- del2_sqlkode.sql
+++ del2_sqlkode.sql
@@ -16,128 +16,72 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `normalisering1`
+-- Dumping data for table `normalisering1`
 --
 
-DROP TABLE IF EXISTS `normalisering1`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering1` (
-  `costumername` varchar(100) NOT NULL,
-  `address` varchar(100) DEFAULT NULL,
-  `phone` smallint DEFAULT NULL,
-  `artist` varchar(50) DEFAULT NULL,
-  `title` varchar(50) DEFAULT NULL,
-  `purchasedate` date DEFAULT NULL,
-  `salesprice` int DEFAULT NULL,
-  PRIMARY KEY (`costumername`),
-  UNIQUE KEY `phone_UNIQUE` (`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering1` WRITE;
+/*!40000 ALTER TABLE `normalisering1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `normalisering1` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `normalisering2_bought`
+-- Dumping data for table `normalisering2_bought`
 --
 
-DROP TABLE IF EXISTS `normalisering2_bought`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering2_bought` (
-  `costumerid` int NOT NULL,
-  `artist` varchar(50) DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL,
-  `purchasedate` date DEFAULT NULL,
-  `salesprice` int DEFAULT NULL,
-  PRIMARY KEY (`costumerid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering2_bought` WRITE;
+/*!40000 ALTER TABLE `normalisering2_bought` DISABLE KEYS */;
+/*!40000 ALTER TABLE `normalisering2_bought` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `normalisering2_costumer`
+-- Dumping data for table `normalisering2_costumer`
 --
 
-DROP TABLE IF EXISTS `normalisering2_costumer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering2_costumer` (
-  `costumerid` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `phone` varchar(50) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`costumerid`),
-  UNIQUE KEY `costumerid_UNIQUE` (`costumerid`),
-  UNIQUE KEY `phone_UNIQUE` (`phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering2_costumer` WRITE;
+/*!40000 ALTER TABLE `normalisering2_costumer` DISABLE KEYS */;
+INSERT INTO `normalisering2_costumer` VALUES (1,'Jackson Elizabeth','(206) 284-6783','123 - 4th Avenue, Fonthill ON, L3J 4S4');
+/*!40000 ALTER TABLE `normalisering2_costumer` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `normalisering3_artist`
+-- Dumping data for table `normalisering3_artist`
 --
 
-DROP TABLE IF EXISTS `normalisering3_artist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering3_artist` (
-  `idartist` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`idartist`),
-  UNIQUE KEY `artistid_UNIQUE` (`idartist`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering3_artist` WRITE;
+/*!40000 ALTER TABLE `normalisering3_artist` DISABLE KEYS */;
+INSERT INTO `normalisering3_artist` VALUES (3,'Carol Channing'),(15,'Dennis Frings');
+/*!40000 ALTER TABLE `normalisering3_artist` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `normalisering3_costumer`
+-- Dumping data for table `normalisering3_costumers`
 --
 
-DROP TABLE IF EXISTS `normalisering3_costumer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering3_costumer` (
-  `idcostumer` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idcostumer`),
-  UNIQUE KEY `costumerid_UNIQUE` (`idcostumer`),
-  CONSTRAINT `normalisering3_paintings` FOREIGN KEY (`idcostumer`) REFERENCES `normalisering3_paintings` (`idpainting`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering3_costumers` WRITE;
+/*!40000 ALTER TABLE `normalisering3_costumers` DISABLE KEYS */;
+INSERT INTO `normalisering3_costumers` VALUES (1,'Jackson Elizabeth','(206) 284-6783','123 - 4th Avenue, Fonthill ON, L3J 4S4'),(2,'Maja Bijedic','12345678','Smedevej 2');
+/*!40000 ALTER TABLE `normalisering3_costumers` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `normalisering3_paintings`
+-- Dumping data for table `normalisering3_paintings`
 --
 
-DROP TABLE IF EXISTS `normalisering3_paintings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering3_paintings` (
-  `idpainting` int NOT NULL AUTO_INCREMENT,
-  `idartist` int DEFAULT NULL,
-  `title` varchar(100) NOT NULL,
-  `purchasedate` date DEFAULT NULL,
-  `salesprice` int NOT NULL,
-  PRIMARY KEY (`idpainting`),
-  UNIQUE KEY `idpainting_UNIQUE` (`idpainting`),
-  KEY `normalisering3_artist_idx` (`idartist`),
-  CONSTRAINT `normalisering3_artist` FOREIGN KEY (`idartist`) REFERENCES `normalisering3_artist` (`idartist`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering3_paintings` WRITE;
+/*!40000 ALTER TABLE `normalisering3_paintings` DISABLE KEYS */;
+INSERT INTO `normalisering3_paintings` VALUES (1,3,'Laugh with Teeth','2000-09-17',7000,1),(2,15,'South toward Emerald Sea','2000-11-05',1800,1),(3,3,'At the Movies','2002-02-14',5550,2),(4,15,'South toward Emerald Sea','2003-07-15',2200,2);
+/*!40000 ALTER TABLE `normalisering3_paintings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Table structure for table `normalisering3_purchase`
+-- Dumping data for table `normalisering3_purchases`
 --
 
-DROP TABLE IF EXISTS `normalisering3_purchase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `normalisering3_purchase` (
-  `idcostumer` int NOT NULL,
-  `idartist` varchar(50) NOT NULL,
-  `title` varchar(45) NOT NULL,
-  `purchasedate` date NOT NULL,
-  PRIMARY KEY (`idcostumer`,`idartist`,`title`,`purchasedate`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `normalisering3_purchases` WRITE;
+/*!40000 ALTER TABLE `normalisering3_purchases` DISABLE KEYS */;
+INSERT INTO `normalisering3_purchases` VALUES ('15',2,'2000-11-05'),('15',4,'2003-07-15'),('3',1,'2000-09-17'),('3',3,'2002-02-14');
+/*!40000 ALTER TABLE `normalisering3_purchases` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -148,4 +92,4 @@ CREATE TABLE `normalisering3_purchase` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-21 23:31:28
+-- Dump completed on 2020-10-22  2:24:08
